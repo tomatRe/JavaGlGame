83d0b8b3a5bb0a089ba388012aa0fd039804fcaf
angel_re <angelre.rebollo@gmail.com>
2020-08-27 13:50:27 +0200

    Added mipmapping support

--- src/RenderEngine/Loader.java
+++ src/RenderEngine/Loader.java
@@ -2,10 +2,7 @@ package RenderEngine;
 
 import Models.RawModel;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
@@ -81,6 +78,9 @@ public class Loader {
 
         try {
             texture = TextureLoader.getTexture("PNG",new FileInputStream("res/"+fileName+".png"));
+            GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+            GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+            GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -1);
         } catch (IOException e) {
             e.printStackTrace();
         }

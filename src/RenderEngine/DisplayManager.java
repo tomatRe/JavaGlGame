ff54abb5c15a14b204639d54ad2379b0761699bb
angel_re <angelre.rebollo@gmail.com>
2020-09-12 20:40:17 +0200

    set the max fps to 0 for debugging reassons

--- src/RenderEngine/DisplayManager.java
+++ src/RenderEngine/DisplayManager.java
@@ -8,7 +8,7 @@ public class DisplayManager {
 
     private static final int WIDTH = 1280;
     private static final int HEIGHT = 720;
-    private static final int maxFps = 120;
+    private static final int maxFps = 0;
 
     private static long lastFrameTime;
     private static float delta;
